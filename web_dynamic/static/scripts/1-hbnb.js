7ccf3dcef1ab80c5b0d4609bdcea361fbdbbcfc3
ER1-53 <cremey.erwan@gmail.com>
2024-02-05 13:55:44 +0100

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	$('input[data-id=":amenity_id"][data-name=":amenity_name"]').on('change', function(){
-		if ($(this).is(":checked")) {
-			$('.amenities > h4').each
-			console.log('mise à jour');
-		} else {
-			console.log('remove');
-		}
+    var amenities = {};  // Créer un objet pour stocker les ID des commodités cochées
+
+    $('input[type="checkbox"]').on('change', function() {
+        var amenityId = $(this).data('id');  // Obtenir l'ID de la commodité
+        var amenityName = $(this).data('name');  // Obtenir le nom de la commodité
+
+        if ($(this).is(':checked')) {
+            // Si la case est cochée, ajouter l'ID de la commodité à l'objet
+            amenities[amenityId] = amenityName;
+        } else {
+            // Si la case est décochée, supprimer l'ID de la commodité de l'objet
+            delete amenities[amenityId];
+        }
+
+        // Mettre à jour le tag h4 avec la liste des commodités cochées
+        var amenitiesList = Object.values(amenities).join(', ');
+        var longueurMax = 35;
+        if (amenitiesList.length > longueurMax) {
+            amenitiesList = amenitiesList.substring(0, longueurMax) + '...';
+        }
+        $('.amenities > h4').text(amenitiesList);
 	});
 });
+
